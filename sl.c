24e1cc576d606592c2aadfb65053e23cef6e8a49
elmira dehghan <elmirdehqan.gmail.com>
2018-11-21 05:46:37 -0800

    This is test

---
+++ sl.c
@@ -0,0 +1,10 @@
+Execute a command 100 seconds hence.
+
+              {sleep 100; command}&
+
+       Repeat a command every 30 seconds.
+
+              while (){
+                   command
+                   sleep 30
+              }
